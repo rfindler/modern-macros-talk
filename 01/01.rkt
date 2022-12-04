571e80fd1e7fabbcb66e5563ddca2c14e58adc63
Robby Findler <robby@racket-lang.org>
2022-12-04 11:59:34 -0600

    try to flesh out the introduction a little bit more

--- 01/01.rkt
+++ 01/01.rkt
@@ -1,15 +1,50 @@
 #lang racket
-(require "../setup.rkt" slideshow "open-compiler.rkt")
+(require "../setup.rkt" slideshow "open-compiler.rkt"
+         pict/shadow)
 
 (define (introduction)
   (slide
    (scale (vc-append
            10
            (t "Modern Macros")
-           (t "vs")
-           (t "an Open Compiler"))
+           (t "as an")
+           (t "Open Compiler"))
           2))
+
+  (slide
+   (scale (vc-append
+           10
+           (tt "#define")
+           (hbl-append 40 (t "cpp") (t "m4"))
+           (blank 0 20)
+           (colorize (t "Not these macros!") "red"))
+          2))
+
+  (slide
+   (lt-superimpose
+    (scale-to-fit (bitmap (build-path paper-images "aim-057.png"))
+                  client-w client-h)
+    (colorize (linewidth 8 (frame (inset (scale (t "From 1963") 1.5) 20 10))) "red")))
+
+  (slide (scale-to-fit (paper 1986) client-w client-h))
+
+  (slide (scale-to-fit (vc-append (hc-append (paper 1986)
+                                             (paper 1990)
+                                             (paper 1992))
+                                  (hc-append (paper 2002)
+                                             (paper 2016)))
+                       client-w client-h))
+  
   (open-compiler))
 
-(module+ main (introduction))
+(define (paper name)
+  (define filename
+    (cond
+      [(number? name)
+       (for/or ([file (in-list (directory-list paper-images))])
+         (and (regexp-match? (~a "^" name) (path->bytes file))
+              file))]
+      [else name]))
+  (shadow-frame (bitmap (build-path paper-images filename))))
 
+(module+ main (introduction))
--- setup.rkt
+++ setup.rkt
@@ -1,4 +1,8 @@
 #lang racket
-(require pict/code slideshow)
+(require pict/code slideshow racket/runtime-path)
+(provide paper-images)
+
 (current-code-font '(bold . "Inconsolata"))
 (current-tt-font '(bold . "Inconsolata"))
+(current-main-font "Lato")
+(define-runtime-path paper-images "paper-images")
