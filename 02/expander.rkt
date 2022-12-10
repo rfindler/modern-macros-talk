88b9cfcca115d14633b8576f481ce16043032ac9
Robby Findler <robby@racket-lang.org>
2022-12-10 12:17:17 -0600

    flesh out expand slides

--- 02/expander.rkt
+++ 02/expander.rkt
@@ -4,32 +4,22 @@
 
 (define (expander)
   (with-title "The Expander"
-    (play-n
-     (λ (n)
+    (slide
+     (vl-append
+      40
+       
+      (hbl-append (t "We need a driver loop; ")
+                  (inset expand-p 8 0)
+                  (t " does that job"))
 
-       (vl-append
-        40
-        (vl-append
-         10
-         (tt "Expand : syntax-object Γ → syntax-object")
-         (hbl-append (t "We need an open driver loop; ")
-                     (inset (tt "expand") 8 0)
-                     (t " does that job")))
-  
+      (vl-append
+       (hbl-append expand-p (tt " : syntax-object (id → val) → syntax-object"))
+       (t "the environment tracks the macros in scope"))
+      
+      (t "There are 3 interesting cases")))
 
-        (cellophane
-         (vl-append
-          10
-          (t "Three interesting cases:")
-          (hc-append (blank 40 0)
-                     (vl-append
-                      10
-                      (t "• a core form")
-                      (t "• a use of a macro")
-                      (t "• a definition of a macro"))))
-         n))))
-
-
+    (expand-cases)
+    #;
     (slide
      (vl-append
       (expand-call (code #'(if e1 e2 e3)))
@@ -43,24 +33,65 @@
                   (hbl-append (code e2′) (t " = ") (expand-call (code e2)))
                   (hbl-append (code e3′) (t " = ") (expand-call (code e3)))))))))
 
-#|
+(define (expand-cases)
+  (define case1
+    (vl-append
+     (expand-call (code #'(if e1 e2 e3)))
+     (t "=")
+     (code #'(if e1′ e2′ e3′))))
+  (define case2
+    (vl-append
+     (expand-call (code #'(m e1 ...)))
+     (t "=")
+     (expand-call (code (λ (stx) e2) #'(m e1 ...)))))
+  (define case3
+    (vl-append
+     (expand-call (code #'(let-syntax ([id proc-e]) body-e)))
+     (t "=")
+     (expand-call (code body-e) (hbl-append (t "Γ + { ") (code id) (t " → ")
+                                            (code (eval #'proc-e))
+                                            (t " }")))))
 
-(expand (or (f x) (g y)))
-=
-(expand (or-proc #'(or (f x) (g y)))
+  (define where1
+    (vl-append
+     (hbl-append (code e1′) (t " = ") (expand-call (code e1)))
+     (hbl-append (code e2′) (t " = ") (expand-call (code e2)))
+     (hbl-append (code e3′) (t " = ") (expand-call (code e3)))))
 
-where
+  (define where2
+    (hbl-append (t "Γ(") (code m) (t ") = ") (code (λ (stx) e2))))
+     
+  (play-n
+   (λ (n1 n2)
+     (define (show p phase)
+       (cellophane
+        p
+        (case phase
+          [(1) (- 1 n1)]
+          [(2) (* n1 (- 1 n2))]
+          [(3) (* n1 n2)])))
+     (vl-append
 
-(define-syntax or (λ (stx) ...))
+      (lt-superimpose (show (t "Case 1: found the IR") 1)
+                      (show (t "Case 2: found a macro") 2)
+                      (show (t "Case 3: found a macro definition") 3))
+      (hc-append
+       (blank 40 0)
+       (lt-superimpose
+        (show case1 1)
+        (show case2 2)
+        (show case3 3)))
 
+      (blank 0 40)
+      (cellophane (t "where") (- 1 n2))
 
-(expand Γ (define-syntax id proc) ...)
-=
-(expand Γ+{id→proc} ...)
+      (hc-append
+       (blank 40 0)
+       
+       (lt-superimpose
+        (show where1 1)
+        (show where2 2)))))))
 
-where ... is what follows in the scope of the define-syntax.
-
-|#
    
 (module+ main (expander))
 
@@ -68,5 +99,5 @@ where ... is what follows in the scope of the define-syntax.
   (parameterize (#;[current-main-font "Zapfino"])
     (tt "Expand")))
 
-(define (expand-call p)
-  (htl-append expand-p (t "⟦ ") p (t " , Γ") (t " ⟧")))
+(define (expand-call p [Γ (t "Γ")])
+  (htl-append expand-p (t "⟦ ") p (t " , ") Γ (t " ⟧")))
