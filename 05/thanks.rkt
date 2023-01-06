b95a424aca54690cd78e58a5ce06a1ff7e94f1c3
Robby Findler <robby@racket-lang.org>
2023-01-06 16:08:50 -0600

    add name highlighting support and highlight sorawee and ryan

--- 05/thanks.rkt
+++ 05/thanks.rkt
@@ -1,7 +1,7 @@
 #lang racket
 (require "../lib/setup.rkt"
          "../lib/title.rkt"
-         slideshow)
+         slideshow slideshow/play)
 
 (provide thanks)
 (define (thanks)
@@ -9,28 +9,61 @@
     (inset (colorize (filled-rectangle (+ client-w margin margin) (+ client-h margin margin))
                      plt-background-color)
            (- margin)))
-  (slide
-   (cc-superimpose
-    (refocus (lt-superimpose
-              bkg
-              (inset plt-title-background 400 300 0 0))
-             bkg)
-    (vl-append
-     60
-     (folks "Macro Paper Authors"
-            "Matthew Flatt" "Matthias Felleisen"
-            "Daniel P. Freidman" "Bruce Duba"
-            "William Clinger" "Jonathan Rees"
-            "R. Kent Dybvig" "Robert Hieb"
-            "Carl Bruggeman" "Mitchell Wand"
-            "Eugene E. Kohlbecker"
-            "Ryan Culpepper" "Michael Ballantyne"
-            "Alexis King" "Timothy P. Hart")
-     (folks "Language Authors"
-            "Sorawee Porncharoenwase"
-            "Jesse Tov" "Spencer Florence"
-            "Stephen Chang" "Alex Knauth"
-            "Sam Tobin-Hochstadt")))))
+  (play-n
+   (λ (n)
+     (define sorawee (mk-pict+string "Sorawee Porncharoenwase"))
+     (define ryan (mk-pict+string "Ryan Culpepper"))
+     (cc-superimpose
+      (refocus (lt-superimpose
+                bkg
+                (inset plt-title-background 400 300 0 0))
+               bkg)
+      (add-background-on-name
+       (add-background-on-name
+        (vl-append
+         60
+         (folks "Macro Paper Authors"
+                "Matthew Flatt" "Matthias Felleisen"
+                "Daniel P. Freidman" "Bruce Duba"
+                "William Clinger" "Jonathan Rees"
+                "R. Kent Dybvig" "Robert Hieb"
+                "Carl Bruggeman" "Mitchell Wand"
+                "Eugene E. Kohlbecker"
+                ryan "Michael Ballantyne"
+                "Alexis King" "Timothy P. Hart")
+         (folks "Language Authors"
+                sorawee
+                "Jesse Tov" "Spencer Florence"
+                "Stephen Chang" "Alex Knauth"
+                "Sam Tobin-Hochstadt"))
+        ryan n)
+       sorawee n)))))
+
+(define (add-background-on-name main p+s n)
+  (define p (t (pict+string-string p+s)))
+  (define w (+ (pict-width p) 20))
+  (define h (+ (pict-height p) 6))
+  (pin-under
+   (pin-over
+    main
+    (pict+string-pict p+s)
+    lt-find
+    (lt-superimpose (cellophane p (- 1 n))
+                    (cellophane (colorize p "white") n)))
+   (pict+string-pict p+s)
+   lt-find
+   (cellophane
+    (refocus (cc-superimpose (inset (colorize (filled-rounded-rectangle w h) "firebrick")
+                                    0 0 0 -2)
+                             (ghost p))
+             p)
+    n)))
+
+(struct pict+string (pict string))
+(define (mk-pict+string string)
+  (pict+string
+   (ghost (t string))
+   string))
 
 (define (folks caption . names)
   (set! names (sort names string<? #:key last-name))
@@ -39,15 +72,21 @@
    (scale (t caption) 2)
    (inset (apply para
                  #:width 680
-                 (add-between names ",")) 40 0 0 0)))
+                 (add-between (map pict+string->pict names) ",")) 40 0 0 0)))
+
+(define (pict+string->pict p)
+  (cond
+    [(pict+string? p) (pict+string-pict p)]
+    [else p]))
 
 (define (last-name str)
+  (when (pict+string? str) (set! str (pict+string-string str)))
   (last (regexp-split #rx" " str)))
 
 (define (add-and names)
   (let loop ([names names])
     (cond
-      [(null? (cdr names)) (list (~a "and " (car names)))]
+      [(null? (cdr names)) (list "and" (car names))]
       [else (cons (car names) (loop (cdr names)))])))
 
 
